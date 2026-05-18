84ac44ae7775f68ae82d6defb4753787b46bc70d
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-05-18 20:30:09 -0300

    Pull de Transaccion a FamiliaTransaccion

--- Transaccion.java
+++ Transaccion.java
@@ -6,14 +6,41 @@
 public class Transaccion{
     private float monto;
     private String descripcion; 
-    private int hora
+    private int hora;
+    Fecha fecha;
 
-    public Transaccion(float monto, String descripcion, int hora){
+    public Transaccion(float monto, Fecha fecha, String descripcion, int hora){
         this.monto = monto;
         this.descripcion = descripcion;
         this.fecha = fecha;
+        this.hora = hora;   
+    }
+    
+    public void setMonto(float monto){
+        this.monto = monto;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setHora(int hora) {
         this.hora = hora;
     }
 
-    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Fecha getFecha() {
+        return fecha;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
 }
