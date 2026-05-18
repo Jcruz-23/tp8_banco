531224a26f018f6189253324ea28da02e90ab672
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-05-18 20:12:53 -0300

    Setters y Getters de Transaccion

---
+++ Transaccion.java
@@ -0,0 +1,19 @@
+/**
+ * Clase Transaccion
+ * @author Sanchez Solano Juan Cruz
+ */
+
+public class Transaccion{
+    private float monto;
+    private String descripcion; 
+    private int hora
+
+    public Transaccion(float monto, String descripcion, int hora){
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    
+}
