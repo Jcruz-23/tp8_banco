7dcafd1052d170149ce175a9193b178e672d909e
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-05-20 19:50:37 -0300

    Excepciones, Eliminacion de Setters (estaban al pedo), cambio de tipo de variable de hora y 'automatizacion' de fecha

--- Transaccion.java
+++ Transaccion.java
@@ -1,19 +1,19 @@
 /**
  * Clase Transaccion
- * @author Sanchez Solano Juan Cruz
+ * @author Sagnchez Solano Juan Cruz
  */
 
 public class Transaccion{
-    private float monto;
+    private Float monto;
     private String descripcion; 
-    private int hora;
+    private Integer[] hora = new Integer[2];
     Fecha fecha;
 
-    public Transaccion(float monto, Fecha fecha, String descripcion, int hora){
-        this.monto = monto;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-        this.hora = hora;   
+    public Transaccion(float monto, String descripcion, int hora, int minuto){
+        setMonto(monto);
+        setDescripcion(descripcion);
+        this.fecha = new Fecha(23, 05, 2026);//Simulacion de fecha automatica"
+        setHora(hora, minuto);   
     }
     
     public void setMonto(float monto){
@@ -21,11 +21,16 @@ public class Transaccion{
     }
 
     public void setDescripcion(String descripcion) {
+        if(descripcion == null) throw new NullPointerException("Error: Descripcion no puede ser nulo");
+        if(descripcion.isBlank()) throw new IllegalArgumentException("Error: Descripcion no puede estar en blanco");
         this.descripcion = descripcion;
     }
 
-    public void setHora(int hora) {
-        this.hora = hora;
+    public void setHora(int hora, int minuto) {
+        if(hora > 23 || hora < 00) throw new IllegalArgumentException("Error: Hora tiene que estar en el rango [00, 23]");
+        if(minuto > 59 || minuto < 0) throw new IllegalArgumentException("Error: Minuto tiene que estar en el rango [00, 59]");
+        this.hora[0] = hora;
+        this.hora[1] = minuto;
     }
 
     public String getDescripcion() {
@@ -42,5 +47,9 @@ public class Transaccion{
 
     public float getMonto() {
         return monto;
+    }
+    
+    public String toString(){        
+        return fecha.toString() + hora[0] + ":" + hora[1] + monto + "'" + descripcion + "'"; 
     }
 }
