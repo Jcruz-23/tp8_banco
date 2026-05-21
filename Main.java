2132ad8ff435cd75b25c4b26f48abe3463a0f6d7
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-05-21 20:59:32 -0300

    Main tentativo

---
+++ Main.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+
+        try(){
+        banco.registrarCliente(45589354, "Maria", 3658.25);
+        banco.registrarCliente(29665741, "Ana", 98714.23);
+        banco.registrarCliente(23587412, "Carlos", 896547.3, "Cajero");
+        banco.registrarEmpleado(45879852, "Juan", 251.3, "Limpieza");
+        banco.registrarEmpleado(21054785, "Carlo", 3.2, "Gerente");
+        banco.cambiarEstado(29665741, "inactivo");
+        banco.listarClientes();
+        banco.listarPersonal();
+        banco.listarTransaccionesCliente(2358741);
+        banco.listarTransaccionesMes(5);
+        banco.TransaccionesAnio(2026);
+        banco.listarTransacciones();
+
+        Cliente c = banco.buscarCliente(45589354);
+        c.realizarTransaccion("pesos", 3000, "deposito");
+        System.out.println("Datos del cliente: " + c.descripcion());
+        }catch(IllegalArgumentException e){
+            System.out.println("Error: " + e.getMessage());
+        }catch(RuntimeException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+}
