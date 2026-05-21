760cf7bf5495f30ec3aea2be6c45936230c9e394
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-05-21 18:20:42 -0300

    Finalizacion de familia de persona

--- Persona.java
+++ Persona.java
@@ -1,49 +1,79 @@
-<<<<<<< HEAD
+/**
+ * Representa una Persona
+ * @author Rocio Garzon
+ * @version 1.0
+ */
 public class Persona{
-=======
-public class Persona {
->>>>>>> 27b1da735889c65e2a8a94cc1b588ecee9721867
  private 
  int dni;
  String nombre;
  int anio_ingreso;
  
- 
+ /**
+  * Constructor de objetos de la clase Persona
+  * @param dni de la persona
+  * @param nombre de la persona 
+  * @param anio_ingreso al sistema
+  */
  public Persona(int dni, String nombre, int anio_ingreso){
     this.dni = dni;
     this.nombre = nombre;
     this.anio_ingreso = anio_ingreso;
  }
 
+ /**
+  * Setea el anio de ingreso
+  * @param anio_ingreso al sistema
+  */
     public void setAnio_ingreso(int anio_ingreso) {
         this.anio_ingreso = anio_ingreso;
     }
 
+    /**
+     * Setea el dni
+     * @param dni de la persona
+     */
     public void setDni(int dni) {
         this.dni = dni;
     }
 
+    /**
+     * Setea el nombre
+     * @param nombre de la persona
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Retorna el anio de ingreso
+     * @return anio_ingreso al sistema
+     */
     public int getAnio_ingreso() {
         return anio_ingreso;
     }
 
+    /**
+     * Retorna el dni
+     * @return dni de la persona
+     */
     public int getDni() {
         return dni;
     }
     
+    /**
+     * Retorna el nombre 
+     * @return nombre de la persona
+     */
     public String getNombre() {
         return nombre;
     }
-<<<<<<< HEAD
-}
-=======
 
-    String toString(){
+    /**
+     * Retorna una cadena de texto con los atributos de persona
+     * @return cadena con atributos ingresados
+     */
+    String ToString(){
         return nombre + ", " + anio_ingreso + ", " + dni;
     }
 }
->>>>>>> 27b1da735889c65e2a8a94cc1b588ecee9721867
--- Personal.java
+++ Personal.java
@@ -1,20 +1,46 @@
+/**
+ * Representa el personal del banco
+ * Hereda el comportamiento de Persona
+ * @author Rocio Garzon
+ * @version 1.0
+ */
 public class Personal extends Persona{
     private String cargo;
 
+    /**
+     * Constructor de objetos de la clase Personal
+     * @param dni del empleado
+     * @param nombre del empleado
+     * @param anio_ingreso al banco
+     * @param cargo que tiene en el banco
+     */
     public Personal(Integer dni, String nombre, Integer anio_ingreso, String cargo){
         super(dni, nombre, anio_ingreso);
         this.cargo = cargo;
     }
 
+    /**
+     * Setea el nombre del cargo del empleado
+     * @param cargo 
+     */
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
+
+    /**
+     * Retorna el cargo del empleado
+     * @return cargo
+     */
     public String getCargo() {
         return cargo;
     }
 
+    /**
+     * Retorna una cadena con las caracteristicas del empleado
+     * @return cadena de datos
+     */
     String Descripcion(){
-        return toString() + ", " + cargo;
+        return ToString() + ", " + cargo;
     }
 }
 
