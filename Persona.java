e41fec74bf7858aa2d423e09b6f5eac5ea87b096
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-05-21 11:34:14 -0300

    Creado de persona otra vez

--- Persona.java
+++ Persona.java
@@ -1,3 +1,41 @@
-public class Persona{
+public class Persona {
+ private 
+ int dni;
+ String nombre;
+ int anio_ingreso;
+ 
+ 
+ public Persona(int dni, String nombre, int anio_ingreso){
+    this.dni = dni;
+    this.nombre = nombre;
+    this.anio_ingreso = anio_ingreso;
+ }
+
+    public void setAnio_ingreso(int anio_ingreso) {
+        this.anio_ingreso = anio_ingreso;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio_ingreso() {
+        return anio_ingreso;
+    }
+
+    public int getDni() {
+        return dni;
+    }
     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String toString(){
+        return nombre + ", dni, anio_ingreso";
+    }
 }
